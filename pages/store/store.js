167583adb2c3b7b6a9e94225dbbef42b929b2137
/ <65301524@qq.com>
2016-09-27 17:00:13 +0800

    banner done

---
+++ pages/store/store.js
@@ -0,0 +1,64 @@
+Page({
+  data:{
+    // text:"这是一个页面"
+    motto: 'Hello World',
+    bannerData: [],
+    listData: []
+  },
+  onLoad:function(options){
+    // 页面初始化 options为页面跳转所带来的参数
+    var self = this;
+    wx.request({
+        url:'http://appwk.baidu.com/nahome/freepromote?na_uncheck=1&opid=wk_na&fr=6&bid=7&wh=146,186',
+        header: {
+            'Content-Type': 'application/json'
+        },
+        success: function(res) {
+           if(res.data.status.code == 0){
+              self.setData({
+                bannerData : res.data.data.promote_area
+              });
+              console.log(self.data.bannerData);
+           }
+        }
+    });
+
+    wx.request({
+        url:'http://appwk.baidu.com/nahome/freecolumn?na_uncheck=1&opid=wk_na&fr=6&bid=7&wh=146,186&summary=1&pn=0&rn=2',
+        header: {
+            'Content-Type': 'application/json'
+        },
+        success: function(res) {
+           if(res.data.status.code == 0){
+              self.setData({
+                listData : res.data.data.column_area
+              });
+              console.log(self.data.listData);
+           }
+        }
+    });
+
+    console.log('onLoad')
+    var that = this
+    //调用应用实例的方法获取全局数据
+    app.getUserInfo(function(userInfo){
+      //更新数据
+      that.setData({
+        userInfo:userInfo
+      })
+    })
+
+  },
+  onReady:function(){
+    // 页面渲染完成
+  },
+  onShow:function(){
+    // 页面显示
+  },
+  onHide:function(){
+    // 页面隐藏
+  },
+  onUnload:function(){
+    // 页面关闭
+  }
+})
